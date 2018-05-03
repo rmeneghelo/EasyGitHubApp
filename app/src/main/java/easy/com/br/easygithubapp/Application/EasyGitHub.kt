ff03bf5105f95c4f9b0668447df59e10a5815a51
Rafael Zambelli Meneghelo <rmeneghelo@gmail.com>
2018-05-02 21:50:13 -0300

    First commit

---
+++ app/src/main/java/easy/com/br/easygithubapp/Application/EasyGitHub.kt
@@ -0,0 +1,13 @@
+package easy.com.br.easygithubapp.Application
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import easy.com.br.easygithubapp.R
+
+class EasyGitHub : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_easy_git_hub)
+    }
+}
