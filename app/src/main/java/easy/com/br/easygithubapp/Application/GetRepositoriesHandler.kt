ddcc0d4b3342c743033bcf9c25073974f3d9c683
Rafael Zambelli Meneghelo <rmeneghelo@gmail.com>
2018-05-06 14:03:49 -0300

    Fixing rx structure

--- app/src/main/java/easy/com/br/easygithubapp/Application/GetRepositoriesHandler.kt
+++ app/src/main/java/easy/com/br/easygithubapp/Application/GetRepositoriesHandler.kt
@@ -1,9 +1,15 @@
 package easy.com.br.easygithubapp.Application
 
+import android.database.Observable
 import android.util.Log
+import easy.com.br.easygithubapp.Model.RepositoriesResult
 import easy.com.br.easygithubapp.Model.Repository
 import easy.com.br.easygithubapp.Repository.GitHubRepository
 import easy.com.br.easygithubapp.Services.RetrofitService
+import io.reactivex.Observable.just
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class GetRepositoriesHandler{
 
@@ -13,16 +19,21 @@ class GetRepositoriesHandler{
         val repository = GitHubRepository(retrofitService)
         var list = listOf<Repository>()
 
-        repository.GetRepositories().subscribe(
-                {
-                    result ->
-                    Log.d("Handler Xuxa", result.size.toString())
-                    list = result
-                },
-                { e ->
-                    Log.d("Erro na view", e.toString())
-                }
-        )
+        repository.GetRepositories()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        {
+                            repositories ->
+                            list = repositories.items
+                            Log.d("Handler Xuxa 1", list.size.toString())
+
+                        },
+                        { error ->
+                            Log.d("Xuxa erro handler", error.message)
+                        }
+                )
+
         Log.d("Handler Xuxa 2", list.size.toString())
         return list
     }
--- app/src/main/java/easy/com/br/easygithubapp/Repository/GitHubRepository.kt
+++ app/src/main/java/easy/com/br/easygithubapp/Repository/GitHubRepository.kt
@@ -2,6 +2,7 @@ package easy.com.br.easygithubapp.Repository
 
 import android.util.Log
 import easy.com.br.easygithubapp.Domain.Interfaces.IGithubRepositoriesService
+import easy.com.br.easygithubapp.Model.RepositoriesResult
 import easy.com.br.easygithubapp.Model.Repository
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -17,26 +18,8 @@ class GitHubRepository{
         retrofitService = retrofit
     }
 
-    fun GetRepositories() : Observable<List<Repository>> {
+    fun GetRepositories() : Observable<RepositoriesResult> {
         val service = retrofitService.create<IGithubRepositoriesService>(IGithubRepositoriesService::class.java)
-        return Observable.create {
-            subscriber ->
-            var listRepository = listOf<Repository>()
-            service.search()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                            { result ->
-                                run {
-                                    Log.d("Xuxa 1", result.items.size.toString())
-                                    listRepository = result.items
-                                }
-                            },
-                            { error -> Log.d("Xuxa 2", error.message) }
-                    )
-            Log.d("Xuxa Repository", listRepository.size.toString())
-            subscriber.onNext(listRepository)
-            subscriber.onComplete()
-        }
+        return service.search()
     }
 }
