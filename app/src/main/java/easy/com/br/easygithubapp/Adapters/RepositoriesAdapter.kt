4843077c2a8a98e96e1914c00e109d775b7d3553
Rafael Zambelli Meneghelo <rmeneghelo@gmail.com>
2018-05-09 00:09:28 -0300

    Adding Picasso

--- app/src/main/java/easy/com/br/easygithubapp/Adapters/RepositoriesAdapter.kt
+++ app/src/main/java/easy/com/br/easygithubapp/Adapters/RepositoriesAdapter.kt
@@ -1,10 +1,13 @@
 package easy.com.br.easygithubapp.Adapters
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import easy.com.br.easygithubapp.Model.Repository
 import easy.com.br.easygithubapp.R
 import kotlinx.android.synthetic.main.github_repository_row.view.*
@@ -18,7 +21,7 @@ class RepositoriesAdapter(items: List<Repository>?, val clickListener: (Reposito
         var githubRepositoryName: TextView = view.githubRepositoryName
         var description: TextView = view.description
         var authorName: TextView = view.authorName
-        var authorPhoto: TextView = view.authorPhoto
+        var authorPhoto: ImageView = view.authorPhoto
         var starsNumber: TextView = view.starsNumber
         var forksNumber: TextView = view.forksNumber
     }
@@ -32,11 +35,18 @@ class RepositoriesAdapter(items: List<Repository>?, val clickListener: (Reposito
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val repository = repositoriesList?.get(position)
+        holder.githubRepositoryName.text = repository?.githubRepositoryName
         holder.description.text = repository?.description
         holder.authorName.text = repository?.owner?.authorName
-        holder.authorPhoto.text = repository?.owner?.authorPhoto
         holder.starsNumber.text = repository?.starsNumber.toString()
         holder.forksNumber.text = repository?.forksNumber.toString()
+        if(repository?.owner?.authorPhoto!!.isNotEmpty())
+        {
+            Picasso
+                    .get()
+                    .load(repository?.owner?.authorPhoto)
+                    .into(holder.authorPhoto)
+        }
     }
 
     override fun getItemCount(): Int = repositoriesList!!.size
