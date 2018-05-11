cf0473de08d5d55f3d91b88e098de1908a9fe4a7
Rafael Zambelli Meneghelo <rmeneghelo@gmail.com>
2018-05-10 22:47:30 -0300

    Create details screen

---
+++ app/src/main/java/easy/com/br/easygithubapp/View/EasyGitHubDetails.kt
@@ -0,0 +1,12 @@
+package easy.com.br.easygithubapp.View
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import easy.com.br.easygithubapp.R
+
+class EasyGitHubDetails: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_easy_git_hub_details)
+    }
+}
