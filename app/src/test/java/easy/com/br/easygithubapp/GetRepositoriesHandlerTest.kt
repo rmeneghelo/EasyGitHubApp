e18c744eff393738651f767d66f4d32fa624a57c
Rafael Zambelli Meneghelo <rmeneghelo@gmail.com>
2018-07-14 23:48:26 -0300

    Create unit tests for GetRepositoriesHandler

--- app/src/test/java/easy/com/br/easygithubapp/GetRepositoriesHandlerTest.kt
+++ app/src/test/java/easy/com/br/easygithubapp/GetRepositoriesHandlerTest.kt
@@ -1,16 +1,17 @@
 package easy.com.br.easygithubapp
 
 import com.google.gson.GsonBuilder
+import easy.com.br.easygithubapp.Model.RepositoriesApiResult
+import easy.com.br.easygithubapp.TestHelper.Companion.API_RESPONSE
 import easy.com.br.easygithubapp.application.GetRepositoriesHandler
 import easy.com.br.easygithubapp.domain.model.RepositoryDto
-import easy.com.br.easygithubapp.Model.RepositoriesApiResult
 import easy.com.br.easygithubapp.repository.GitHubRepository
-import easy.com.br.easygithubapp.TestHelper.Companion.API_RESPONSE
 import io.reactivex.Observable
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.observers.TestObserver
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.Schedulers
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,6 +19,7 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.runners.MockitoJUnitRunner
+import java.util.concurrent.TimeoutException
 
 @RunWith(MockitoJUnitRunner::class)
 class GetRepositoriesHandlerTest {
@@ -42,10 +44,8 @@ class GetRepositoriesHandlerTest {
     }
 
     @Test
-    fun addition_isCorrect() {
+    fun `when repositories are requested, call getRepositories and return repositoriesDto`() {
         Mockito.`when`(repository.getRepositories()).thenReturn(Observable.just(apiResult))
-
-        getRepositoriesHandler.getRepositories()
 
         val testObserver = TestObserver<RepositoryDto>()
 
@@ -53,11 +53,68 @@ class GetRepositoriesHandlerTest {
                 .repositoriesResult
                 .subscribe(testObserver)
 
+        getRepositoriesHandler.getRepositories()
+
         testObserver.assertComplete()
         testObserver.assertNoErrors()
-        //testObserver.assertValueCount(1)
-        //val listResult = testObserver.values()[0]
+        testObserver.assertValueCount(1)
 
-        //Assert.assertEquals(listResult.listRepositories.size, 2)
+        val listResult = testObserver.values()[0]
+
+        Assert.assertEquals(listResult.listRepositories.size, 2)
+        Assert.assertEquals(listResult.listRepositories[0].owner.authorName, "iluwatar")
+        Assert.assertEquals(listResult.listRepositories[1].owner.authorName, "ReactiveX")
+    }
+
+    @Test
+    fun `when repositories are requested, call getRepositories and return no results return repositoriesDto`() {
+        apiResult = RepositoriesApiResult(0, arrayListOf())
+        Mockito.`when`(repository.getRepositories()).thenReturn(Observable.just(apiResult))
+
+        val testObserver = TestObserver<RepositoryDto>()
+
+        getRepositoriesHandler
+                .repositoriesResult
+                .subscribe(testObserver)
+
+        getRepositoriesHandler.getRepositories()
+
+        testObserver.assertComplete()
+        testObserver.assertNoErrors()
+        testObserver.assertValueCount(1)
+
+        val listResult = testObserver.values()[0]
+
+        Assert.assertEquals(listResult.listRepositories.size, 0)
+    }
+
+    @Test
+    fun `when repositories are requested, call getRepositories and return exception`() {
+        Mockito.`when`(repository.getRepositories()).thenReturn(Observable.error(Exception()))
+
+        val testObserver = TestObserver<RepositoryDto>()
+
+        getRepositoriesHandler
+                .repositoriesResult
+                .subscribe(testObserver)
+
+        getRepositoriesHandler.getRepositories()
+
+        testObserver.assertError(Exception::class.java)
+    }
+
+    @Test
+    fun `when repositories are requested, call getRepositories and return timeout`() {
+        Mockito.`when`(repository.getRepositories()).thenReturn(Observable.error(TimeoutException()))
+
+        val testObserver = TestObserver<RepositoryDto>()
+
+        getRepositoriesHandler
+                .repositoriesResult
+                .subscribe(testObserver)
+
+        getRepositoriesHandler.getRepositories()
+
+        testObserver.assertError(TimeoutException::class.java)
     }
 }
