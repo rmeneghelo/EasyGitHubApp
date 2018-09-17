54256d34b42acb7a091a9f445bef654511020d70
Rafael Zambelli Meneghelo <rafael.meneghelo@MacBook-Pro-de-Administrador.local>
2018-09-17 10:33:36 -0300

    Change to mockito kotlin

--- app/src/test/java/easy/com/br/easygithubapp/GetRepositoriesViewModelTest.kt
+++ app/src/test/java/easy/com/br/easygithubapp/GetRepositoriesViewModelTest.kt
@@ -12,7 +12,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.runners.MockitoJUnitRunner
+import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Call
 import retrofit2.Response
 
